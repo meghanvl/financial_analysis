9e4f8aebf5d7eb311ef4ae8ef21f236f2377016e
meghanvl <meghanvluoma@gmail.com>
2020-11-11 22:30:09 -0600

    11.11.20 changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,25 +7,47 @@ import csv
 csvpath = os.path.join("Resources", "budget_data.csv")
 
 total_months = []
-total_profloss = []
+total_amount = 0
+average_change = []
+greatest_increase = 0
+greatest_decrease = 0
+change_month = 0
 
 with open(csvpath) as csv_file:
    print(csv_file)
    csv_reader = csv.reader(csv_file, delimiter=",")
 
-   next(csv_file)
+   next(csv_reader)
+   firstrow = next(csv_reader)
+   total_amount = total_amount + int(firstrow[1])
+   previous_month = int(firstrow[1])
 
    for row in csv_reader:
-           print(row)
+        total_months.append(row[0])
+        total_amount += int(row[1])
+        change = int(row[1]) - previous_month
+        previous_month = int(row[1])
+        average_change.append(change)
 
-           total_months.append(row[0])
-           total_profloss.append(row[0])
+        if change > greatest_increase:
+                greatest_increase = change
+                increase_month = row[0]
 
-print(f"Total Months: {len(total_months)}")
+        if change < greatest_decrease:
+                greatest_decrease = change
+                decrease_month = row[0]
 
+average = round(sum(average_change) / len(average_change) , 2)     
 
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {len(total_months) + 1 }")
+print(f'Total: ${total_amount}')
+print(f'Average Change: ${average}')
+print(f'Greatest Increase in Profits: {increase_month} (${greatest_increase})')
+print(f'Greatest Decrease in Profits: {decrease_month} (${greatest_decrease})')
 
+output_file = os.path.join("output.txt")
 
-        #append total month and profit
-        # total_month.append(row[0])
-        # total_profit.append(row[0])
+with open(output_file, "w", newline='') as txt_file:
+    writer = csv.writer(datafile)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,16 +1,32 @@
 import os
 import csv
 
-csvpath = os.path.join("Resources", "election.csv")
+csvpath = os.path.join("Resources", "election_data.csv")
 
-total_count = []
+vote_count = 0
 candidates = []
+candidate_vote = {}
 
 with open(csvpath) as csv_file:
-   print(csv_file)
+   
    csv_reader = csv.reader(csv_file, delimiter=",")
 
    next(csv_file)
 
    for row in csv_reader:
-           print(row)
+    
+        vote_count += 1
+        candidate_name = row[2]
+        if candidate_name not in candidates:
+            candidates.append(candidate_name) 
+            candidate_vote[candidate_name] = 0
+        candidate_vote[candidate_name] = candidate_vote[candidate_name] + 1 #adding 1 to the candidate_vote count each time name is found
+# print(candidate_vote, candidates, vote_count)
+
+for candidate in candidates:
+    votes = candidate_vote.get(candidate)
+    percent_votes = round(votes / vote_count *100, 2)
+    print(percent_votes)
+
+
+
