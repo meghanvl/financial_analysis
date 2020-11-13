a4512acf17b94007032da5afabb2a6ff7651cb8c
meghanvl <meghanvluoma@gmail.com>
2020-11-13 14:42:08 -0600

    commit on main

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,6 +5,7 @@ import csv
 #create path for load
 csvpath = os.path.join("Resources", "budget_data.csv")
 
+#initialize variables
 total_months = []
 total_amount = 0
 average_change = []
@@ -14,17 +15,17 @@ change_month = 0
 
 #open file to read
 with open(csvpath) as csv_file:
-   print(csv_file)
    csv_reader = csv.reader(csv_file, delimiter=",")
 
    #skip the header row     
    next(csv_reader)
 
-   #skip January
+   #skip Jan and store value in separate variable to compare to Feb and so on
    firstrow = next(csv_reader)
    total_amount = total_amount + int(firstrow[1])
    previous_month = int(firstrow[1])
 
+   #loop through file to calulate total months, total amount and average change 
    for row in csv_reader:
         total_months.append(row[0])
         total_amount += int(row[1])
@@ -32,6 +33,7 @@ with open(csvpath) as csv_file:
         previous_month = int(row[1])
         average_change.append(change)
 
+        #calculate average increase and decrease in profits/losses
         if change > greatest_increase:
                 greatest_increase = change
                 increase_month = row[0]
@@ -40,6 +42,7 @@ with open(csvpath) as csv_file:
                 greatest_decrease = change
                 decrease_month = row[0]
 
+#calculate average change
 average = round(sum(average_change) / len(average_change) , 2)     
 
 print("Financial Analysis")
@@ -50,6 +53,7 @@ print(f'Average Change: ${average}')
 print(f'Greatest Increase in Profits: {increase_month} (${greatest_increase})')
 print(f'Greatest Decrease in Profits: {decrease_month} (${greatest_decrease})')
 
+#output text file to Analysis folder
 txtpath = os.path.join("Analysis", "analysis.summary.txt")
 
 with open(txtpath, "w") as file:
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,8 +1,11 @@
+#import dependencies
 import os
 import csv
 
+#create path for load
 csvpath = os.path.join("Resources", "election_data.csv")
 
+#initialize variables
 vote_count = 0
 candidates = []
 candidate_vote = {}
@@ -12,22 +15,26 @@ Correy_vote = 0
 Li_vote = 0
 Otooley_vote = 0
 
+#open file for reading
 with open(csvpath) as csv_file:
-   
    csv_reader = csv.reader(csv_file, delimiter=",")
 
+   #skip the header row 
    next(csv_reader)
 
+   #loop through to find total vote count and if candidates name is not in candidate list add it 
    for row in csv_reader:
         vote_count += 1
         candidate_name = row[2]
+
         if candidate_name not in candidates:
             candidates.append(candidate_name) 
             candidate_vote[candidate_name] = 0
 
-        #adding 1 to the candidate_vote count each time name is found    
+        #loop through and add 1 to the candidate_vote count each time candidate's name is found    
         candidate_vote[candidate_name] = candidate_vote[candidate_name] + 1
 
+        #calculate total votes for each candidate
         if candidate_name == "Khan":
             Khan_vote += 1
         elif candidate_name == "Correy":
@@ -37,17 +44,17 @@ with open(csvpath) as csv_file:
         else:
             Otooley_vote += 1
 
+            #find the average and total votes for each candidate
             Khan_percent = round(Khan_vote / vote_count * 100, 2)
             Correy_percent = round(Correy_vote / vote_count *100, 2)
             Li_percent = round(Li_vote / vote_count * 100, 2)
             Otooley_percent = round(Otooley_vote / vote_count * 100, 2)
-    
-            
 
 for candidate in candidates:
     votes = candidate_vote.get(candidate)
     percent_votes = round(votes / vote_count * 100)
-    
+
+    #find the candidate with the most votes and assign to winner candidate variable 
     if votes > winner_vote:
         winner_vote = votes
         winner_candidate = candidate
@@ -65,8 +72,10 @@ print("----------------------------")
 print(f"Winner: {winner_candidate}")
 print("----------------------------")
 
+#output to Analysis folder
 txtpath = os.path.join("Analysis", "election.summary.txt")
 
+#send to text file
 with open(txtpath, "w") as file:
     file.write("Election Results" + "\n")
     file.write("----------------------------" + "\n")
